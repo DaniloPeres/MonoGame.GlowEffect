391578af6907bde1ba8654d887296f400d7991ad
Danilo Peres <danilo_meirelles@hotmail.com>
2021-07-08 23:41:53 +0200

    Improve the transparent pixel

--- MonoGame.GlowEffect/GlowEffectSpriteFont.cs
+++ MonoGame.GlowEffect/GlowEffectSpriteFont.cs
@@ -5,6 +5,8 @@ namespace MonoGame
 {
     public static partial class GlowEffect
     {
+        public static Texture2D transparentPixel;
+
         public static Texture2D CreateGlowSpriteFont(SpriteFont spriteFont, string text, Color textColor, Color glowColor, Vector2 scale, float blurX, float blurY, float dist, float angle, float alpha, float strength, float inner, float knockout, float circleSamplingTimes, float linearSamplingTimes, GraphicsDevice graphics)
         {
             var textTexture2D = DrawSpriteFontToTexture2D(spriteFont, text, textColor, scale, graphics);
@@ -13,9 +15,12 @@ namespace MonoGame
 
         private static Texture2D DrawSpriteFontToTexture2D(SpriteFont spriteFont, string text, Color textColor, Vector2 scale, GraphicsDevice graphics)
         {
+            var textSize = spriteFont.MeasureString(text) * scale;
+            if (textSize.X == 0 || textSize.Y == 0)
+                return GetTransparentPixel(graphics);
+
             lock (graphics)
             {
-                var textSize = spriteFont.MeasureString(text) * scale;
                 var target = new RenderTarget2D(graphics, (int)textSize.X, (int)textSize.Y);
                 using (var spriteBatch = new SpriteBatch(graphics))
                 {
@@ -28,6 +33,19 @@ namespace MonoGame
                     return target;
                 }
             }
+        }
+
+        private static Texture2D GetTransparentPixel(GraphicsDevice graphics)
+        {
+            if (transparentPixel == null)
+            {
+                transparentPixel = new Texture2D(graphics, 1, 1);
+                var color = new Color[1];
+                color[0] = Color.Transparent;
+                transparentPixel.SetData<Color>(color);
+            }
+
+            return transparentPixel;
         }
     }
 }
